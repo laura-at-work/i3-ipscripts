6a76bef507653fd8d2405daf86cf3b75327382ad
Matthias Kalb <matthias.kalb@brz.eu>
2020-07-23 11:46:37 +0200

    changed ip color scheme

--- get-ip.sh
+++ get-ip.sh
@@ -10,13 +10,15 @@ fi
 ipresponse=`/usr/sbin/ip -f inet addr show $interface 2> /dev/null | grep -Po 'inet \K[\d.]+'`
 
 if [[ $ipresponse == *"."*"."*"."* ]]; then
+    fgcolor="#daf7A6"
     ip=$ipresponse
 else
+    fgcolor="#ff5733"
     ip="down"
 fi
 
 echo "$interface: $ip"
 echo "$interface: $ip"
-# echo "#112233"   # foreground color
+echo "$fgcolor"   # foreground color
 # echo "#00ff00"   # background color
 exit 0
